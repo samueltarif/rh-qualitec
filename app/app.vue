37db0c037e51c8a7194c36d9f390f8101de93d66
SAMUEL TARIF <samuel.tarif@gmail.com>
2025-12-19 05:52:13 -0300

    debug: versao minima app.vue para identificar erro 500

--- app/app.vue
+++ app/app.vue
@@ -1,24 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
     <NuxtPage />
-    <ToastContainer />
   </div>
 </template>
 
 <script setup lang="ts">
-// TEMPORÁRIO: Removido initAuth para debug do erro 500
-// TODO: Reativar após corrigir problema de SSR
-
-// Inicializar autenticação apenas no client-side
-onMounted(async () => {
-  try {
-    if (process.client) {
-      const { initAuth } = useAppAuth()
-      await initAuth()
-    }
-  } catch (error) {
-    console.error('Erro ao inicializar auth:', error)
-  }
-})
+// VERSÃO MÍNIMA PARA DEBUG - Removido tudo que pode causar SSR issues
 </script>
